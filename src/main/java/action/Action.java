10a2d98cb309d835d28d6000bb281c7cc37dc649
xxoyeongg <101491440+xxoyeongg@users.noreply.github.com>
2022-07-05 16:02:42 +0900

    db->JDBCutil//////action->Action//////vo->ActionForward

--- src/main/java/action/Action.java
+++ src/main/java/action/Action.java
@@ -1,5 +1,9 @@
 package action;
 
-public interface Action {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public interface Action {
+	ActionForward execute(HttpServletRequest request,HttpServletResponse response)
+	throws Exception;
 }
