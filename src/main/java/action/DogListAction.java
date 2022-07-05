b00758c84a72f3288b3c304bac10f8f172488fdb
suwonBastion <67666965+suwonBastion@users.noreply.github.com>
2022-07-05 17:49:38 +0900

    07.05

--- src/main/java/action/DogListAction.java
+++ src/main/java/action/DogListAction.java
@@ -1,5 +1,33 @@
 package action;
 
-public class DogListAction {
+import java.util.ArrayList;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import svc.DogListService;
+import vo.ActionForward;
+import vo.Dog;
+
+public class DogListAction implements Action{
+	@Override
+	public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception{
+		ArrayList<String> todayImageList = new ArrayList<String>();
+		Cookie[] cookieArray = request.getCookies();
+		
+		if(cookieArray != null) {
+			for(int i=0;i<cookieArray.length;i++) {
+				if(cookieArray[i].getName().startsWith("today")) {
+					todayImageList.add(cookieArray[i].getName());
+				}
+			}
+		}
+		DogListService dogListService = new DogListService();
+		ArrayList<Dog> dogList = dogListService.getDogList();
+		request.setAttribute("DogList", dogList);
+		request.setAttribute("todayImageList",todayImageList);
+		ActionForward forward = new ActionForward("dogList.jsp",false);
+		return forward;
+	}
 }
